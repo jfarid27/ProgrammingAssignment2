1cefa5d0943c0e419b6f5c1d84d3d1a7bff51852
jfarid27 <jrab227@gmail.com>
2014-12-21 11:14:27 -0500

    Added cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Requirement for ginv generalized inverse used later
+library('MASS')
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheMatrix <- function(x =NULL){
+    # [Matrix] => cacheMatrix    //EBNF signature
+    # This function takes a matrix and returns a cacheMatrix
+    # object. It acts as a closure to store matrix values.
+    # Note that the requirements given in the assignment are not
+    # necessary since it is possible to have a single object to
+    # cache and store the matrix.
+    
+    #This sets up the outer environment variables. Notice that x is optional
+    #I've also used .'s to identify the actual variables between
+    #the function getters and setters for the variables.
+    .matrix <- x
+    .inverse <- NULL
+    
+    matrix <- function(y){
+        # []
+        
+        if (!missing(y)){
+            #This is the case where we're given the matrix to set
+            .matrix <<- y 
+            
+            return(exports)     
+            #This is cool for method chaining, if you're into it.
+        } 
+        
+        #If we weren't given a matrix to set, the user wants the matrix
+        #back, so we return that.
+        return(.matrix)
+    }
+    
+    inverse <- function(){
+        if(is.null(.inverse)){
+            #This is the case where the inverse wasn't computed yet, so
+            # we compute it
+            .inverse <- ginv(x)
+            #and return it
+            return(.inverse)
+        }
+        
+        #Out here, .inverse wasn't null, so we return it's value since it's
+        #already been cached.
+        return(.inverse)
+    }
+    
+    exports <- list(matrix=matrix, inverse=inverse)
+    
+    return(exports)
+    
+    
 }
